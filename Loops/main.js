920f79a661f9ac995b40e60c9f87294fff70c694
Jaedyn <61067221+Jcarbonell04@users.noreply.github.com>
2020-06-02 13:18:06 -0600

    Add files via upload

---
+++ Loops/main.js
@@ -0,0 +1,26 @@
+// Number Patterns
+
+// DOM Elements
+let outputEl = document.getElementById("output");
+
+// While Loop to print a pattern to the output element
+// let num = 1;// Initalize
+// while (num <= 12) { // Test
+//     outputEl.innerHTML += num + "<br>"; // Body
+//     num++; // Update
+// }
+
+// For Loop to print a pattern to the output element
+// for (let num = 1; num <15; num++);
+// outputEl.innerHTML += num + "<br>";
+
+// outputEl.innerHTML += "1<br>";
+// outputEl.innerHTML += "2<br>";
+// outputEl.innerHTML += "3<br>";
+// outputEl.innerHTML += "4<br>";
+// outputEl.innerHTML += "5<br>";
+// outputEl.innerHTML += "6<br>";
+// outputEl.innerHTML += "7<br>";
+// outputEl.innerHTML += "8<br>";
+// outputEl.innerHTML += "9<br>";
+// outputEl.innerHTML += "10<br>";
